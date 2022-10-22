a6228197ebfcd6ecefb1d25b00cbdbcad504d293
Pratham Agrawal <prathamagrawal1205@gmail.com>
2022-10-22 23:33:33 +0530

    Time: 131 ms (24.43%), Space: 13.9 MB (98.75%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        l, r = 0, len(arr)-1
+        while l <= r:
+            mid = l + (r-l)//2
+            if arr[mid]-mid-1 < k:
+                l = mid+1
+            else:
+                r = mid-1
+            
+        return l + k
